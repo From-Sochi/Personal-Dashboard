396a94e960ccca2a4e42c118fb4d39cf2ff14358
Евгений <blinov.e123@gmail.com>
2025-08-25 22:34:58 +0300

    Настроил зависимости

--- my-dashboard/src/App.tsx
+++ my-dashboard/src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      Hello!
+    </>
   );
 }
 
 export default App;
+
+
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/dashboard" element={<Dashboard />} />
+//         <Route path="/about" element={<About />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
